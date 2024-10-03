769b375ca8d1e958f57555053a0230e5defb433f
gpchub <gpc_dev@hotmail.com>
2024-10-03 10:59:01 +0700

    pxrem bỏ bảng, thêm tailwind css

--- app/Views/converter/pxrem.php
+++ app/Views/converter/pxrem.php
@@ -22,24 +22,13 @@
                     <span class="error-message" x-show="!tailwind_classes.includes(tailwind)">class chưa tồn tại</span>
                 </div>
             </form>
+            <div class="text-center help-text mt-3">Tính theo font size mặc định của browser là 1rem = 16px</div>
         </div>
 
         <div class="card">
-            <h2 class="mb-0">Bảng chuyển đổi</h2>
-            <div>Tính theo font size mặc định của browser là 16px</div>
-            <div class="table-conversion">
-                <template x-if="conversion_table.length">
-                    <template x-for="item in conversion_table">
-                        <div class="table-conversion__item grid grid-cols-3 gap-2">
-                            <span x-text="item.px"></span>
-                            <span x-text="item.rem"></span>
-                            <span x-text="item.tailwind"></span>
-                        </div>
-                    </template>
-                </template>
-
-            </div>
-        </div>
+            <h3>Css Width & Height</h3>
+            <my-code-block x-text="css"></my-code-block>
+        </h3>
     </section>
 <?= $this->endSection() ?>
 
@@ -53,14 +42,18 @@
             rem: 1,
             tailwind: 4,
             tailwind_classes: [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 96],
+            css: '',
 
             conversion_table: [],
 
             init() {
+                this.generateCss();
+
                 this.$watch('px', (value, oldValue) => {
                     if ( value != oldValue ) {
                         this.rem = this.px / 16;
                         this.tailwind = this.px / 4;
+                        this.generateCss();
                     }
                 } );
 
@@ -68,6 +61,7 @@
                     if ( value != oldValue ) {
                         this.px = this.rem * 16;
                         this.tailwind = this.rem * 4;
+                        this.generateCss();
                     }
                 } );
 
@@ -75,17 +69,60 @@
                     if ( value != oldValue ) {
                         this.px = this.tailwind * 4;
                         this.rem = this.tailwind / 4;
+                        this.generateCss();
                     }
                 } );
-
-                for (let i = 2; i <= 1000; i += 2) {
-                    this.conversion_table.push({
-                        px: i + 'px',
-                        rem: (i / 16) + 'rem' ,
-                        tailwind: 'w-' + (i / 4),
-                    })
-                }
             },
+
+            generateCss() {
+                let breakpoints = {
+                    md: 768,
+                    lg: 992,
+                }
+
+                let selectors = {
+                    'w': 'width',
+                    'h': 'height',
+                    'min-w': 'min-width',
+                    'min-h': 'min-height',
+                    'max-w': 'max-width',
+                    'max-h': 'max-height',
+                };
+
+                let output = [];
+                let css = [];
+
+                for (const item in selectors) {
+                    css.push(this.createCssRule(item, selectors[item], this.tailwind));
+                }
+
+                output.push(css.join("\n"));
+
+                for (const bp in breakpoints) {
+                    css = [`@media (min-width: ${breakpoints[bp]}px) {`];
+
+                    for (const item in selectors) {
+                        css.push("\t" + this.createCssRule(item, selectors[item], this.tailwind, bp));
+                    }
+
+                    css.push('}');
+                    output.push(css.join("\n"));
+                }
+
+                this.css = output.join("\n\n");
+            },
+
+            createCssRule(selector, property, number, breakpoint = '') {
+                let suffix = number.toString().replace('.', '\\.');
+                selector = `${selector}-${suffix}`;
+                if (breakpoint) {
+                    selector = `${breakpoint}\\:${selector}`;
+                }
+                const px = number * 4;
+                const rem = number / 4;
+
+                return `.${selector} { ${property}: ${rem}rem; /* ${px}px */ }\n`;
+            }
 
         }));
     })
